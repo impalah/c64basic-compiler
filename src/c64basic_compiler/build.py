8526e9185a2445b54c3c1294b82c52bce7d5a8f2
impalah <impalah@gmail.com>
2025-04-27 17:30:13 +0200

    Added bas examples. Improved build

--- src/c64basic_compiler/build.py
+++ src/c64basic_compiler/build.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 from c64basic_compiler.compiler.tokenizer import tokenize
 from c64basic_compiler.compiler.parser import parse
 from c64basic_compiler.compiler.codegen import generate_code
@@ -7,11 +8,39 @@ from c64basic_compiler.compiler.prg_writer import write_prg
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Use: python build.py <file.bas>")
-        return
+    parser = argparse.ArgumentParser(
+        description="BASIC Compiler for  C64 (C64BASIC Compiler)."
+    )
+    parser.add_argument("-i", "--input", required=True, help="Input .bas file")
+    parser.add_argument("-o", "--output", required=True, help="Output .prg file")
+    parser.add_argument(
+        "-f", "--force", action="store_true", help="Overwrite output file if it exists"
+    )
+    args = parser.parse_args()
 
-    input_file = sys.argv[1]
+    input_file = args.input
+    output_file = args.output
+    force_overwrite = args.force
+
+    if not os.path.isfile(input_file):
+        print(f"Error: Input file '{input_file}' does not exist.")
+        sys.exit(1)
+
+    # Create folders if they do not exist
+    output_dir = os.path.dirname(output_file)
+    if output_dir:
+        os.makedirs(output_dir, exist_ok=True)
+
+    # Check if the output file already exists
+    if os.path.isfile(output_file) and not force_overwrite:
+        confirm = (
+            input(f"file '{output_file}' already exists. Overwrite? (y/N): ")
+            .strip()
+            .lower()
+        )
+        if confirm != "n":
+            print("Operation cancelled.")
+            sys.exit(1)
 
     with open(input_file, "r") as f:
         source = f.read()
@@ -20,10 +49,8 @@ def main():
     ast = parse(tokens)
     binary = generate_code(ast)
 
-    # TODO: allow to specify output file name and folder
-    os.makedirs("out", exist_ok=True)
-
-    write_prg("out/prg_output.prg", binary)
+    write_prg(output_file, binary)
+    print(f"Binary file PRG succesfully generated: {output_file}")
 
 
 if __name__ == "__main__":
